ba7e1382a3e88b089c56cfe6c647df62f1bd2862
Kumar Bibek <coomar.101@gmail.com>
2014-01-12 10:44:51 +0530

    Fix for KITKAT new Document Picker

--- src/com/kbeanie/imagechooser/threads/ImageProcessorThread.java
+++ src/com/kbeanie/imagechooser/threads/ImageProcessorThread.java
@@ -26,88 +26,85 @@ import com.kbeanie.imagechooser.api.ChosenImage;
 
 public class ImageProcessorThread extends MediaProcessorThread {
 
-	private final static String TAG = "ImageProcessorThread";
+    private final static String TAG = "ImageProcessorThread";
 
-	private ImageProcessorListener listener;
+    private ImageProcessorListener listener;
 
-	private final static int MAX_DIRECTORY_SIZE = 5 * 1024 * 1024;
+    private final static int MAX_DIRECTORY_SIZE = 5 * 1024 * 1024;
 
-	private final static int MAX_THRESHOLD_DAYS = (int) (0.5 * 24 * 60 * 60 * 1000);
+    private final static int MAX_THRESHOLD_DAYS = (int) (0.5 * 24 * 60 * 60 * 1000);
 
-	public ImageProcessorThread(String filePath, String foldername,
-			boolean shouldCreateThumbnails) {
-		super(filePath, foldername, shouldCreateThumbnails);
-		setMediaExtension("jpg");
-	}
+    public ImageProcessorThread(String filePath, String foldername, boolean shouldCreateThumbnails) {
+        super(filePath, foldername, shouldCreateThumbnails);
+        setMediaExtension("jpg");
+    }
 
-	public void setListener(ImageProcessorListener listener) {
-		this.listener = listener;
-	}
+    public void setListener(ImageProcessorListener listener) {
+        this.listener = listener;
+    }
 
-	public void setContext(Context context) {
-		this.context = context;
-	}
+    public void setContext(Context context) {
+        this.context = context;
+    }
 
-	@Override
-	public void run() {
-		try {
-			manageDiretoryCache(MAX_DIRECTORY_SIZE, MAX_THRESHOLD_DAYS, "jpg");
-			processImage();
-		} catch (IOException e) {
-			e.printStackTrace();
-			if (listener != null) {
-				listener.onError(e.getMessage());
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			if (listener != null) {
-				listener.onError(e.getMessage());
-			}
-		}
-	}
+    @Override
+    public void run() {
+        try {
+            manageDiretoryCache(MAX_DIRECTORY_SIZE, MAX_THRESHOLD_DAYS, "jpg");
+            processImage();
+        } catch (IOException e) {
+            e.printStackTrace();
+            if (listener != null) {
+                listener.onError(e.getMessage());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (listener != null) {
+                listener.onError(e.getMessage());
+            }
+        }
+    }
 
-	private void processImage() throws Exception {
-		// Picasa on Android >= 3.0
-		if (filePath != null && filePath.startsWith("content:")) {
-			filePath = getAbsoluteImagePathFromUri(Uri.parse(filePath));
-		}
-		if (filePath == null || TextUtils.isEmpty(filePath)) {
-			if (listener != null) {
-				listener.onError("Couldn't process a null file");
-			}
-		} else if (filePath.startsWith("http")) {
-			downloadAndProcess(filePath);
-		} else if (filePath
-				.startsWith("content://com.google.android.gallery3d")) {
-			processPicasaMedia(filePath, ".jpg");
-		} else if (filePath
-				.startsWith("content://com.google.android.apps.photos.content")) {
-			processGooglePhotosMedia(filePath, ".jpg");
-		} else {
-			process();
-		}
-	}
+    private void processImage() throws Exception {
+        // Picasa on Android >= 3.0
+        if (filePath != null && filePath.startsWith("content:")) {
+            filePath = getAbsoluteImagePathFromUri(Uri.parse(filePath));
+        }
+        if (filePath == null || TextUtils.isEmpty(filePath)) {
+            if (listener != null) {
+                listener.onError("Couldn't process a null file");
+            }
+        } else if (filePath.startsWith("http")) {
+            downloadAndProcess(filePath);
+        } else if (filePath.startsWith("content://com.google.android.gallery3d")) {
+            processPicasaMedia(filePath, ".jpg");
+        } else if (filePath.startsWith("content://com.google.android.apps.photos.content")
+                || filePath.startsWith("content://com.android.providers.media.documents")) {
+            processGooglePhotosMedia(filePath, ".jpg");
+        } else {
+            process();
+        }
+    }
 
-	@Override
-	protected void process() throws Exception {
-		super.process();
-		if (shouldCreateThumnails) {
-			String[] thumbnails = createThumbnails(this.filePath);
-			processingDone(this.filePath, thumbnails[0], thumbnails[1]);
-		} else {
-			processingDone(this.filePath, this.filePath, this.filePath);
-		}
-	}
+    @Override
+    protected void process() throws Exception {
+        super.process();
+        if (shouldCreateThumnails) {
+            String[] thumbnails = createThumbnails(this.filePath);
+            processingDone(this.filePath, thumbnails[0], thumbnails[1]);
+        } else {
+            processingDone(this.filePath, this.filePath, this.filePath);
+        }
+    }
 
-	@Override
-	protected void processingDone(String original, String thumbnail,
-			String thunbnailSmall) {
-		if (listener != null) {
-			ChosenImage image = new ChosenImage();
-			image.setFilePathOriginal(original);
-			image.setFileThumbnail(thumbnail);
-			image.setFileThumbnailSmall(thunbnailSmall);
-			listener.onProcessedImage(image);
-		}
-	}
+    @Override
+    protected void processingDone(String original, String thumbnail, String thunbnailSmall) {
+        if (listener != null) {
+            ChosenImage image = new ChosenImage();
+            image.setFilePathOriginal(original);
+            image.setFileThumbnail(thumbnail);
+            image.setFileThumbnailSmall(thunbnailSmall);
+            listener.onProcessedImage(image);
+        }
+    }
 }
